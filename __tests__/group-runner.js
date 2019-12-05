84949120da5f1eeed292510b49946f67149118f0
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-12-05 10:02:18 +0200

    Restructured GroupRunner class and written a test to check getGroups method

---
+++ __tests__/group-runner.js
@@ -0,0 +1,35 @@
+const { random, lorem } = require( 'faker' );
+
+const GroupRunner = require( '../index' );
+
+describe( 'GroupRunner', () => {
+	test( '.getGroups', () => {
+		const jestArgs = [
+			'--bail',
+			'--cache',
+			'--changedSince',
+			'--ci',
+			'--clearCache',
+			'--listTests',
+			'--notify',
+			'--showConfig',
+			'--updateSnapshot',
+			'--useStderr',
+		];
+
+		const argv = [ '/usr/bin/node', '/path/to/node_modules/.bin/jest' ];
+		for ( let i = 0, num = random.number( { min: 0, max: jestArgs.length } ); i < num; i++ ) {
+			argv.push( jestArgs[i] );
+		}
+
+		const groups = [];
+		for ( let i = 0, num = random.number( { min: 1, max: 4 } ); i < num; i++ ) {
+			const group = lorem.slug();
+			argv.push( `--group=${ group }` );
+			groups.push( group );
+		}
+
+		const runner = new GroupRunner();
+		expect( runner.getGroups( argv ) ).toEqual( groups );
+	} );
+} );
--- index.js
+++ index.js
@@ -7,29 +7,48 @@ const ARG_PREFIX = '--group=';
 
 class GroupRunner extends TestRunner {
 
-	runTests( tests, watcher, onStart, onResult, onFailure, options ) {
+	getGroups( args ) {
 		const groups = [];
-		process.argv.forEach( ( arg ) => {
+
+		args.forEach( ( arg ) => {
 			if ( arg.startsWith( ARG_PREFIX ) ) {
 				groups.push( arg.substring( ARG_PREFIX.length ) );
 			}
 		} );
 
-		const filteredTests = groups.length === 0 ? tests : tests.filter( ( test ) => {
-			const parsed = parse( fs.readFileSync( test.path, 'utf8' ) );
-			if ( parsed.group ) {
-				const parsedGroup = Array.isArray( parsed.group ) ? parsed.group : [ parsed.group ];
-				for ( let i = 0, len = parsedGroup.length; i < len; i++ ) {
-					if ( typeof parsedGroup[i] === 'string' && groups.find( ( group ) => parsedGroup[i].startsWith( group ) ) ) {
-						return true;
-					}
+		return groups;
+	}
+
+	filterTest( groups, test ) {
+		const parsed = parse( fs.readFileSync( test.path, 'utf8' ) );
+		if ( parsed.group ) {
+			const parsedGroup = Array.isArray( parsed.group ) ? parsed.group : [ parsed.group ];
+			for ( let i = 0, len = parsedGroup.length; i < len; i++ ) {
+				if ( typeof parsedGroup[i] === 'string' && groups.find( ( group ) => parsedGroup[i].startsWith( group ) ) ) {
+					return true;
 				}
 			}
+		}
 
-			return false;
-		} );
+		return false;
+	}
 
-		return super.runTests( filteredTests, watcher, onStart, onResult, onFailure, options );
+	filterTests( args, tests ) {
+		const groups = this.getGroups( args );
+		return groups.length
+			? tests.filter( this.filterTest.bind( this, groups ) )
+			: tests;
+	}
+
+	runTests( tests, watcher, onStart, onResult, onFailure, options ) {
+		return super.runTests(
+			this.filterTests( process.argv, tests ),
+			watcher,
+			onStart,
+			onResult,
+			onFailure,
+			options,
+		);
 	}
 
 }
