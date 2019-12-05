ffac2fcff77782ab375c238ce4f7620d8ac42c40
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-12-05 10:53:11 +0200

    Added a new test to check filterTests method

--- __tests__/group-runner.js
+++ __tests__/group-runner.js
@@ -2,33 +2,42 @@ const { random, lorem } = require( 'faker' );
 
 const GroupRunner = require( '../index' );
 
-describe( 'GroupRunner', () => {
-	test( '.getGroups', () => {
-		const jestArgs = [
-			'--bail',
-			'--cache',
-			'--changedSince',
-			'--ci',
-			'--clearCache',
-			'--listTests',
-			'--notify',
-			'--showConfig',
-			'--updateSnapshot',
-			'--useStderr',
-		];
+const jestArgs = [
+	'--bail',
+	'--cache',
+	'--changedSince',
+	'--ci',
+	'--clearCache',
+	'--listTests',
+	'--notify',
+	'--showConfig',
+	'--updateSnapshot',
+	'--useStderr',
+];
 
-		const argv = ['/usr/bin/node', '/path/to/node_modules/.bin/jest'];
-		for ( let i = 0, num = random.number( { min: 0, max: jestArgs.length } ); i < num; i++ ) {
-			argv.push( jestArgs[i] );
-		}
+function getBaseCliArgs( num = 0 ) {
+	const argv = ['/usr/bin/node', '/path/to/node_modules/.bin/jest'];
+	for ( let i = 0; i < num; i++ ) {
+		argv.push( jestArgs[i] );
+	}
 
-		const groups = [];
-		for ( let i = 0, num = random.number( { min: 1, max: 4 } ); i < num; i++ ) {
-			const group = lorem.slug();
-			argv.push( `--group=${ group }` );
-			groups.push( group );
-		}
+	return argv;
+}
 
-		expect( GroupRunner.getGroups( argv ) ).toEqual( groups );
-	} );
+test( 'getGroups', () => {
+	const groups = [];
+	const argv = getBaseCliArgs( random.number( { min: 0, max: jestArgs.length } ) );
+	for ( let i = 0, num = random.number( { min: 1, max: 4 } ); i < num; i++ ) {
+		const group = lorem.slug();
+		argv.push( `--group=${ group }` );
+		groups.push( group );
+	}
+
+	expect( GroupRunner.getGroups( argv ) ).toEqual( groups );
+} );
+
+test( 'filterTests - empty groups', () => {
+	const argv = getBaseCliArgs();
+	const tests = random.words( random.number( { min: 1, max: 10 } ) );
+	expect( GroupRunner.filterTests( argv, tests ) ).toEqual( tests );
 } );
